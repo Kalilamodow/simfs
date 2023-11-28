382dce1ddaeb715d59d422b12c2351ef80a30057
Kalilamodow <86890138+Kalilamodow@users.noreply.github.com>
2023-11-28 00:26:23 +0000

    Implement file permission support for File class Stll need to add perms for Directory class

--- src/classes.py
+++ src/classes.py
@@ -1,15 +1,18 @@
+'''Classes'''
+
 from __future__ import annotations
+from typing import Literal
 from exceptions import InvalidFilenameError
 
 
 INVALID_FILE_CHARS = ['/', '\\', '|', '?', ':', '*', '<', '>', '"']
 
+
 def str_path(src: str|Path) -> Path:
     '''Converts an str to a Path if the type is an str'''
     if isinstance(src, str):
         return Path(src)
-    else:
-        return src
+    return src
 
 
 class Path:
@@ -111,27 +114,157 @@ class Path:
     @property
     def sroot(self): return self._sroot
 
+class Permissions:
+    '''Note: permissions are nine characters long, not ten.'''
+    __str: str
+    __num: int
+    def __init__(self, rawperm: str|int) -> None:
+        valid = self.validate(rawperm)
+        if isinstance(rawperm, int):
+            rawperm = self.numerical_to_str(rawperm)
+
+        if not valid:
+            raise ValueError(f'Please enter a valid permission string ({valid[1]}]')
+        
+        self.__str = rawperm
+        self.__num = self.str_to_numerical(rawperm)
+    
+
+    def validate(self, pstr: str|int) -> tuple[bool, Literal['valid', 'length', 'invalid char', 'duplicate chars']]:
+        '''Validate a permission string'''
+        if isinstance(pstr, int):
+            # easier to validate as int :)
+            if len(str(pstr)) != 3: return False, 'length'
+
+            possible = (0, 1, 2, 4)
+            for c in str(pstr):
+                if c not in possible:
+                    return False, 'invalid char'
+            
+            return True, 'valid'
+
+        # check length
+        if len(pstr) != 9:
+            return False, 'length'
+        
+        # validate characters
+        VALID_CHARS = ('r', 'w', 'x', '-')
+        for c in pstr:
+            if c not in VALID_CHARS:
+                return False, 'invalid char'
+        
+        # split into groups and validate
+        grouped = [list(group) for group in zip(*[iter(list(pstr))] * 3)]
+        for group in grouped:
+            # filter out -s
+            group = list(filter(lambda x: x != '-', group))
+            # check if has duplicates
+            if len(group) == len(set(group)):
+                return False, 'duplicate chars'
+        
+        return True, 'valid'
+
+    def numerical_to_str(self, pInt: int) -> str:
+        '''Turns the numerical representation of a Permission to the string representation'''
+        parts = [int(x) for x in list(str(pInt))]
+        res = ''
+        for numRep in parts:
+            if numRep == 0:
+                res += '-' * 3
+                continue
+
+            for p in range(3):
+                if numRep >= 4 and p == 0:
+                    res += 'r'
+                    numRep -= 4
+                elif numRep >= 2 and p == 1:
+                    res += 'w'
+                    numRep -= 2
+                elif numRep >= 1 and p == 2:
+                    res += 'x'
+                    numRep -= 1
+                else:
+                    res += '-'
+
+        return res
+        
+    def str_to_numerical(self, pstr: str) -> int:
+        '''Converts the string representation of a permission to the numerical representation'''
+        numParts = [0, 0, 0]
+        strParts = [list(group) for group in zip(*[iter(list(pstr))] * 3)]
+        for idx, strPart in enumerate(strParts):
+            for pe in strPart:
+                if pe == 'r':
+                    numParts[idx] += 4
+                    continue
+                if pe == 'w':
+                    numParts[idx] += 2
+                    continue
+                if pe == 'x':
+                    numParts[idx] += 1
+        return int(''.join([str(x) for x in numParts]))
+
+    def has_permission(self, perm: Literal['r', 'w', 'x', 'read', 'write', 'execute', 4, 2, 1],
+                       user: Literal['user', 'group', 'other', 'u', 'g', 'o'] = 'user') -> bool:
+        pidx = None
+        if perm in ['r', 4, 'read']:
+            pidx = 0
+        elif perm in ['w', 2, 'write']:
+            pidx = 1
+        elif perm in ['x', 1, 'execute']:
+            pidx = 2
+
+        uidx = 0
+        # don't bother adding a check for 'user' because it's the default
+        if user in ['group', 'g']:
+            uidx = 1
+        elif user in ['other', 'o']:
+            uidx = 2
+
+        # evaluate using string representation
+        # list looks like this [['r', 'w', 'x'] * 3]
+        grouped = [list(group) for group in zip(*[iter(list(self.__str))] * 3)]
+        return grouped[uidx][pidx] != '-'
+
+
+    def __str__(self, mode='string'):
+        if mode == 'string':
+            return self.__str
+        elif mode == 'num':
+            return str(self.__num)
+        
+    def __int__(self): return self.__num
+
 
 class File:
-    '''Contains metadata (`name` and `path`) and the content. (`content`)'''
+    '''Contains metadata (`name`, `path`, and `permissions`) and the content. (`content`)
+    
+    Note: permissions are mostly for exports, and not really used when navigating within the SimulatedFilesystem.'''
     name: str
+    permissions: Permissions
     contents: bytes
     exists: bool
 
-    def __init__(self, path: Path, contents: bytes|None=None):
+    def __init__(self, path: Path, contents: bytes=b'Def', perms: Permissions|str|int = 'rw-r--r--'):
+        if not isinstance(perms, Permissions):
+            perms = Permissions(perms)
+
         name = path[-1]
         if any(x in name for x in INVALID_FILE_CHARS):
             raise InvalidFilenameError
         
         self.name = name
-        self.contents = contents if contents is not None else ''.encode()
+        self.contents = contents
+        self.permissions = perms
         self.exists = True
 
 
     def write(self, contents: bytes, append=False) -> None:
         '''Write `contents` to this file. If `append`, it will append the contents instead of
         rewriting the whole thing'''
-        self.raiseErrorIfNotExists()
+        self.raiseErrorIfNotExists()       
+        self.checkPermRaiseError("write")
+
 
         if not append:
             self.contents = contents
@@ -140,15 +273,24 @@ class File:
 
     def read(self) -> bytes:
         self.raiseErrorIfNotExists()
+        self.checkPermRaiseError("read")
+
         return self.contents
     
     def readAsString(self) -> str:
         self.raiseErrorIfNotExists()
+        self.checkPermRaiseError("read")
+        print(self.permissions)
+
         return self.contents.decode()
 
     def raiseErrorIfNotExists(self):
         if not self.exists: raise FileNotFoundError(self.name)
     
+    def checkPermRaiseError(self, perm: Literal['read', 'write', 'execute']):
+        if not self.permissions.has_permission(perm):
+            raise PermissionError((f'{self.name} does not have {perm} permissions'))
+
     def delete(self):
         '''Deletes the File'''
         self.contents = None
@@ -177,14 +319,14 @@ class Directory:
         '''Lists files (`self.children`) in the directory as a list of strings.'''
         return [x.name for x in self.children]
     
-    def create_file(self, fname: str, contents: bytes|None=None) -> File:
+    def create_file(self, fname: str, contents: bytes|None=None, permissions: Permissions|str|int=644) -> File:
         '''Creates a file'''
         # make sure doesn't exist already
         if fname in [n.name for n in self.children]:
             raise FileExistsError
         
         # ok then create
-        fi = File(Path(str(self.path) + fname), contents)
+        fi = File(Path(str(self.path) + fname), contents, permissions)
 
         self.children.append(fi)
         return fi
